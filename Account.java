1026c9a35a736f93286470ab25c71e9dd4b2425c
Rashmith Reddy <Rashmith@Rashmiths-MacBook-Pro.local>
2015-07-06 18:58:11 +0100

    first commit

---
+++ Account.java
@@ -0,0 +1,20 @@
+package accounts;
+
+public class Account {
+	
+	
+	
+
+	public static void main() {
+		
+		methodOverloading obj = new methodOverloading ();
+		
+		obj.overloading(25);
+		obj.overloding(2,25);
+	}
+	
+
+
+	
+
+}
