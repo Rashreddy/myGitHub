a07dd532dabe73b1f4b1e777bb062abaf8a9f19c
Rashmith Reddy <rash_mou@yahoo.com>
2015-08-18 22:24:52 +0100

    adding src file

---
+++ src/test/java/tests/TestRunner.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		format = {"pretty" , "html:target/cucumber" ,"json:target/cucumberSampleTests.json"},
+		features = {"target/test-classes/features/login.feature"} )
+public class TestRunner {
+
+}
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,7 @@
+Feature: To login on Gmail Website
+  
+Scenario : Login with Valid Credentials
+  
+  Given i on the Gmail Webpage
+  When i click on login button
+  Then i should login
